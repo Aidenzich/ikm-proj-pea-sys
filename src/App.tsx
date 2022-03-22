6cc4d47513eaf81ff8aed21f6bc7b759101a1468
azen <aidenzich0221@gmail.com>
2022-03-22 16:45:43 +0800

    add search & similarity order

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 
-import {Card, Row, Col, Form} from 'react-bootstrap';
+import {Card, Row, Col, Form, InputGroup, FormControl, Button} from 'react-bootstrap';
 import { MyToolTipContent } from './components/myTooltip';
 import { MyInfo } from './components/myInfo';
 import { MyNav } from './components/myNav';
 import { Gantt, Task, ViewMode } from 'gantt-task-react';
-import { loadData } from './dataLoader';
+import { loadData } from './helpers/dataLoader';
 import React, { useCallback, useEffect }  from 'react';
 import './App.css';
 import "gantt-task-react/dist/index.css";
@@ -18,6 +18,16 @@ function App() {
     
   // 讀取在背景的資料
   const [allTasks, setAllTasks] = React.useState<Task[]>(loadData());
+
+  // 顯示的資料
+  const [displayTasks, setDisplayTasks] = React.useState<Task[]>([]);  
+
+  // 展開的計畫id
+  const [expandedProj, setexpandedProj] = React.useState<Task[]>([]);
+  const [curTask, setCurTask] = React.useState<Task>();
+
+  const [searchString, setSearchString] = React.useState<string>('');  
+
 
   // 只有在更換category的狀況下，allTasks才會變動。當allTask變動時，從新設定顯示的tasks
   const getProjects = useCallback(()=>{
@@ -33,8 +43,8 @@ function App() {
   // 以 project 取得底下的 task
   const getTasks = useCallback((project: Task): Task[] => {
     const tasks = [];
-    if (project !== undefined){      
-      for (var i=0; i< allTasks.length; i++){        
+    if (project !== undefined){
+      for (var i=0; i< allTasks.length; i++){
         if (allTasks[i].project === project.id) {
           tasks.push(allTasks[i]);
         };
@@ -43,15 +53,11 @@ function App() {
     return tasks;
   }, [allTasks])
 
-  // 顯示的資料
-  const [displayTasks, setDisplayTasks] = React.useState<Task[]>(getProjects());  
-  // 展開的計畫id
-  const [expandedProj, setexpandedProj] = React.useState<Task[]>([]);
-  const [curTask, setCurTask] = React.useState<Task>();  
+  
 
   useEffect(()=>{
-    // alert("change")
-    setDisplayTasks(getProjects());
+    resetDisplayedTask();
+    // setDisplayTasks(getProjects());
   }, [allTasks, getProjects]);
 
   // 控制展開項
@@ -59,7 +65,7 @@ function App() {
     let diplayed: Task[] = [...getProjects()];      
     for (let p=0; p < expandedProj.length; ++p){
       let temp: Task[] = getTasks(expandedProj[p]);        
-      Array.prototype.push.apply(diplayed, temp);    
+      Array.prototype.push.apply(diplayed, temp);
     }      
     setDisplayTasks(diplayed);
   }, [expandedProj, getProjects, getTasks]);
@@ -69,11 +75,12 @@ function App() {
       if (expandedProj.filter(ep=> ep.id === task.id).length > 0){
         // 當計畫已展開，重新觸發即移除
         // NOTE: https://stackoverflow.com/questions/62943332/useeffect-not-getting-trigger-after-state-change
-        const temp = [...expandedProj];
-        let rmProjIndex = temp.indexOf(task);        
-        temp.splice(rmProjIndex , 1);
-        setexpandedProj(temp);                
-      } else {        
+        const tempid = expandedProj.map(e => e.id);
+        const temp = [...expandedProj]
+        let rmProjIndex = tempid.indexOf(task.id);        
+        temp.splice(rmProjIndex  , 1);
+        setexpandedProj(temp);
+      } else {
         if (!expandedProj.includes(task)){
           const temp = [...expandedProj];
           temp.push(task);
@@ -85,40 +92,85 @@ function App() {
     setCurTask(task);
   };
 
+  const changeEvent = (event: any) => {
+    setexpandedProj([])    
+    setAllTasks(loadData(event.target.value))    
+  }
 
-  function changeEvent(event: any){    
-    if (event.target.value === '1'){
-      setAllTasks(loadData());
-    } else {
-      setAllTasks(loadData(false));
-      setDisplayTasks(getProjects());
+  const searchTaskName = (searchInput: string) => {
+    let searchTasks = allTasks.filter(t=> t.name.includes(searchInput));
+    let searchTaskId: any[];
+    let searchProjId: any[];
+    searchProjId = [];
+    searchTaskId = [];
+    for (let i=0; i< searchTasks.length; ++i){
+      if (!searchProjId.includes(searchTasks[i].project)){
+        searchProjId.push(searchTasks[i].project)
+      }
+      searchTaskId.push(searchTasks[i].id)
     }
+    
+    console.log(searchProjId)
+    let searchProj = allTasks.filter(p=> 
+      searchProjId.includes(p.id) && !searchTaskId.includes(p.id)
+    );
+    Array.prototype.push.apply(searchTasks, searchProj);
+    
+    setDisplayTasks(searchTasks);
+  }
+
+  const resetDisplayedTask = ()=>{
+    setDisplayTasks(getProjects());
   }
 
   return (
-    <div className="App" style={app_style}>            
+    <div className="App" style={app_style}>
       <header className="App-header">
         <MyNav/>        
         <Row className="card-margin-top m-auto align-self-center">
-          <Col style={{paddingTop:'2vh'}}>
+          <Col style={{paddingTop:'2vh'}}>            
             <Card className="m-auto" style={{ width:"auto", maxWidth:"1200px", borderRadius: "20px",}}>
-              <div style={{margin:"5px", marginTop:"20px"}}>                
-                <Form.Select aria-label="" style={{maxWidth:"1100px", margin: "auto"}} onChange={changeEvent}>                  
-                  <option value="1">20 Category</option>
-                  <option value="2">30 Category</option>                  
-                </Form.Select>
-              </div>
+              <Row style={{margin:"20px 0px 10px 40px"}}>
+                <Col>
+                  <div >                
+                  <Form.Select aria-label="" style={{maxWidth:"1100px", margin: "auto"}} onChange={changeEvent}>                  
+                    <option value="20">20 Category</option>
+                    <option value="30">30 Category</option>
+                    <option value="40">40 Category</option>
+                  </Form.Select>
+                </div>
+                </Col>
+                <Col>
+                <InputGroup className="mb-3">
+                  <FormControl
+                    placeholder="Search"
+                    aria-label="Search"
+                    aria-describedby="Search"
+                    onChange={e=>setSearchString(e.target.value)}
+                  />
+                  <Button id="Search" variant="primary" onClick={()=>searchTaskName(searchString)}>
+                    Search
+                  </Button>
+                  <Button variant="dark" onClick={()=>resetDisplayedTask()}>
+                    Reset
+                  </Button>
+                </InputGroup>
+                </Col>
+              </Row>
+
               <Card.Body className="m-auto  align-self-center">                                                                                  
                 <div className="p-auto" style={{width:"auto", minWidth:"100eh", maxWidth:"90vw"}}>
-                <Gantt
-                  tasks={displayTasks}
-                  viewMode={ViewMode.Month}          
-                  columnWidth={10}
-                  listCellWidth={""}                  
-                  TooltipContent={MyToolTipContent}
-                  onDoubleClick={handleExpanderClick}
-                  ganttHeight={550}
-                />
+                {
+                  (displayTasks.length === 0 ? "empty": <Gantt
+                    tasks={displayTasks}
+                    viewMode={ViewMode.Month}          
+                    columnWidth={10}
+                    listCellWidth={""}                  
+                    TooltipContent={MyToolTipContent}
+                    onDoubleClick={handleExpanderClick}
+                    ganttHeight={550}
+                  />)
+                }
                 </div>
               </Card.Body>
             </Card>
--- src/data/handler.py
+++ src/data/handler.py
@@ -102,6 +102,92 @@ def cleanAndSaveJson(filename):
                 }
                 proj_data.append(p_json)
     
+
+
+    l_jsonString = json.dumps(label_data)
+    p_jsonString = json.dumps(proj_data)
+
+    with open(os.path.join(DATAPATH, f"main_{filename.replace('.csv', '')}.json"), "w") as jf:
+        jf.write(l_jsonString)
+    with open(os.path.join(DATAPATH, f"proj_{filename.replace('.csv', '')}.json"), "w") as jf:
+        jf.write(p_jsonString)
+
+def cleanAndSaveJsonWithOrder(filename):
+    filepath = DATA_PATH / filename
+    df = pd.read_csv(filepath)
+    for c in df.columns.tolist():
+        if df[c].dtype == "object" and c != 'description':
+            df[c] = df[c].apply(lambda x: str(x).replace('[', '').replace(']', '').replace("'", ''))
+
+    df['description'] = df['description'].apply(lambda x: str(x).replace('_x000D_', ''))
+
+    df.to_csv(filepath, index=False)
+    df.fillna(" ", inplace=True)
+
+    df = df[pd.to_numeric(df['year'], errors='coerce').notnull()]
+    df['year'] = pd.to_numeric(df['year'])
+    df['year_end'] = pd.to_numeric(df['year_end'])    
+    df.sort_values(by=['label'], ascending=False, inplace=True)
+
+    label_data = []
+    proj_data = []
+
+    label_list = df['label'].unique().tolist()
+    
+    # label_list = sorted(label_list, key= lambda x: str(x).isnumeric())
+
+    TOTAL_YEARS = df.year.value_counts().sort_index().index.tolist()
+    proj_id = 0    
+    
+
+    # 類別資料
+    for main_id, l in enumerate(label_list):        
+        label_df = df[df['label']==l]    
+        year_list = label_df['year'].unique()
+        project_id = f"main_{main_id}"
+        label_order = label_df['order'].min() - 1
+
+        count_years = init_count_years(TOTAL_YEARS)
+
+        temp_year_count = dict(label_df['year'].value_counts().sort_index())
+        
+        # 取出該類別計畫各年份比數計算
+        for y in temp_year_count.keys():        
+            count_years[int(y)] = int(temp_year_count[y])
+
+        l_json = {
+            "start":int(min(year_list)),
+            "end":int(max(year_list))+1,
+            "name":l,
+            "id": project_id,
+            "displayOrder": label_order,
+            "series": list(count_years.values())
+        }
+
+        label_data.append(l_json)
+        label_df.sort_values(by=['year'], inplace=True)
+        
+        # 建立regular expression 移除 (?/4) 字串的compile
+        r = re.compile(r"\x28[^\x29]+\x29")
+
+                                                    
+        for _, row in label_df.iterrows():
+            proj_id+=1
+            p_json = {
+                "start": int(row['year']),
+                "end": int(row['year']) +1,
+                "name":row['name'],
+                "id":f"proj_{proj_id}",
+                "displayOrder":int(row['order']),
+                "project": project_id,
+                "keyword": row['keyword'] if (row['keyword'] and row['keyword'].strip())  else "無",
+                "ner": row['ner'] if (row['ner'] and row['ner'].strip()) else "無",
+                "tf_idf": row['tf_idf'] if (row['tf_idf'] and row['tf_idf'].strip()) else "無",
+                "desp": row['description'] if (row['description'] and row['description'].strip()) else "無",
+            }
+            proj_data.append(p_json)
+        
+    label_data = sorted(label_data, key=lambda x: x['displayOrder'])
     l_jsonString = json.dumps(label_data)
     p_jsonString = json.dumps(proj_data)
 
@@ -111,6 +197,11 @@ def cleanAndSaveJson(filename):
         jf.write(p_jsonString)
 
 
+
+
 if __name__ == '__main__':
-    cleanAndSaveJson('result20.csv')
-    cleanAndSaveJson('result30.csv')
+    # cleanAndSaveJson('result20.csv')
+    # cleanAndSaveJson('result30.csv')
+    cleanAndSaveJsonWithOrder('final40.csv')
+    cleanAndSaveJsonWithOrder('final20.csv')
+    cleanAndSaveJsonWithOrder('final30.csv')
